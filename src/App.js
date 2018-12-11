16aed64d83a10a06735d281ddb6d7de2fee6c066
Luis Henrique Carvalho do Nascimento <lhcnascimento@gmail.com>
2018-12-11 09:36:52 -0200

    Minimun requirements

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBook from './SearchBook.js'
 import ListBooks from './ListBooks.js'
 
 class BooksApp extends React.Component {
   state = {
+    books: []
   }
+
+  moveBook = (book, shelf) => {
+    if (this.state.books) {
+      BooksAPI.update(book,shelf).then(() => {
+        book.shelf = shelf;
+        console.log(this.state.books.filter(b => b.id !== book.id));
+        console.log(book);
+        this.setState(state => ({
+          books: state.books.filter(b => b.id !== book.id).concat([ book ])
+        }));
+        console.log(this.state.books.filter(b => b.id !== book.id));
+      })
+    }
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then(books => {
+        this.setState({books});
+    });
+}
 
   render() {
     return (
         <div className="app">
-          <Route exact path="/" component={ListBooks} />
-          <Route path="/search" component={SearchBook} />
+          <Route exact path="/" render={() => (
+            <ListBooks
+              onMoveBook={this.moveBook}
+              booksOnShelf={this.state.books} />
+          )} />
+          <Route path="/search" render={() => (
+            <SearchBook
+              onMoveBook={this.moveBook}
+              booksOnShelf={this.state.books}
+            />
+          )}/>
         </div>
     )
   }
--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import PropTypes from 'prop-types';
+import Book from './Book.js';
 
 class SearchBook extends Component {
+
+    static propTypes = {
+        booksOnShelf: PropTypes.array,
+        onMoveBook: PropTypes.func.isRequired
+    }
+
+    state = {
+        query: '',
+        books: []
+    }
+
+    updateQuery = (query) => {
+    if (!query) {
+        this.setState({query: '', books: []})
+    } else {
+        this.setState({ query: query.trim() })
+        BooksAPI.search(query).then((books) => {
+        if (books.error) {
+            books = []
+        }
+        books.map(book => (this.props.booksOnShelf.filter((b) => b.id === book.id).map(b => book.shelf = b.shelf)))
+        this.setState({books})
+        })
+    }
+    }
 
     render(){
         return(
@@ -11,20 +39,22 @@ class SearchBook extends Component {
                         <button className="close-search">Close</button>
                     </Link>
                 <div className="search-books-input-wrapper">
-                    {/*
-                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                    You can find these search terms here:
-                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                    you don't find a specific author or title. Every search is limited by search terms.
-                    */}
-                    <input type="text" placeholder="Search by title or author"/>
-
+                    <input type="text" placeholder="Search by title or author"
+                        onChange={(e) => this.updateQuery(e.target.value)}/>
                 </div>
                 </div>
                 <div className="search-books-results">
-                <ol className="books-grid"></ol>
+                <ol className="books-grid">
+                {this.state.books
+                  .map(book => (
+                    <Book 
+                      onMoveBook={this.props.onMoveBook}
+                      key={book.id}
+                      book={book}
+                    />
+                  ))
+                }
+                </ol>
                 </div>
             </div>
         )
